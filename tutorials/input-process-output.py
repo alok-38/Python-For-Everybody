b455426bacdb57b10727b9fb9fa361500fa19ca6
ThePrimeagen <the.primeagen@aol.com>
2026-06-22 09:27:55 +0530

    Working with input and output

---
+++ tutorials/input-process-output.py
@@ -0,0 +1,10 @@
+# 1. Inputs go in the parentheses (Arguments)
+def greet_user(username):
+    # Processing happens inside
+    greeting = f"Hello, {username}!"
+    # Output is sent back
+    return greeting
+
+# Using the function
+message = greet_user("Alok")
+print(message)
